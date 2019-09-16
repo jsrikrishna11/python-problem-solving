762596c2bba851cdf04e96757636b85107a2c4e3
jsrikrishna11 <41172782+jsrikrishna11@users.noreply.github.com>
2019-09-16 18:40:51 +0530

    Update serialize.py

--- serialize.py
+++ serialize.py
@@ -2,7 +2,7 @@ class Node:
     def __init__(self,key=-1): 
         self.left = None
         self.right = None
-        self.val =  -1
+        self.val =  key
   
   
 # A function to do inorder tree traversal 
@@ -17,8 +17,22 @@ def serialize(root):
 def deSerialize(root, s):
     root.val = s[0]
     if(s[1]!=-1):
+        root.left = Node()
         deSerialize(root.left, s[1:])
     if(s[2]!= -1):
+        root.right = Node
         deSerialize(root.right,s[2:])
-    return
+    return root
+
+if __name__ == '__main__':
+    root = Node(key=1)
+    root.left      = Node(key=2) 
+    root.right     = Node(key=3) 
+    root.left.left  = Node(key=4) 
+    root.left.right  = Node(key=5)
+    serialize(root)
+    # serial = [1,2,4,-1,-1,5,-1,-1,3,-1,-1]
+    # newRoot = Node(key=10)
+    # newRoot = deSerialize(newRoot,serial)
+    # serialize(newRoot)
  
